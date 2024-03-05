b54559f4529d3add7da06aaa24e93b9ca7e954ad
kbruneto <hizztxt@gmail.com>
2024-03-04 21:40:30 -0300

    att

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const port = 3000
-const uuid = require('uuid')
 
+const uuid = require('uuid')
+import cors from 'cors'
+
+const port = 3001
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const orders = []
 
